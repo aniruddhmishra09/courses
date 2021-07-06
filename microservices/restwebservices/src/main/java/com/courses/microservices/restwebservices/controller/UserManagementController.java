4fa81d1c455be3c0a7ced8b0d6d7d326b4d4f8b6
aniruddhmishra09 <aniruddhmishra09@gmail.com>
2021-07-06 11:14:19 +0530

    Response Entity added in controller

--- microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/controller/UserManagementController.java
+++ microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/controller/UserManagementController.java
@@ -3,14 +3,17 @@
  */
 package com.courses.microservices.restwebservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.courses.microservices.restwebservices.request.UserRequest;
 import com.courses.microservices.restwebservices.response.UserResponse;
@@ -39,7 +42,10 @@ public class UserManagementController {
 	}
 	
 	@PostMapping(path = "/user")
-	public UserResponse createUser(@RequestBody UserRequest userRequest) {
-		return userManagementService.createUser(userRequest);
+	public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
+		UserResponse userResponse = userManagementService.createUser(userRequest);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userResponse.getId()).toUri();
+		return ResponseEntity.created(location).build();
+		
 	}
 }
