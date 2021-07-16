53b0c4cb27467c3a587fb7b7aa440596e2d2eabe
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2021-07-16 09:13:26 +0530

    Adding i18n Feature

--- microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/controller/GreetingsController.java
+++ microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/controller/GreetingsController.java
@@ -3,6 +3,9 @@
  */
 package com.courses.microservices.restwebservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,24 +21,33 @@ import com.courses.microservices.restwebservices.response.GreetingsResponse;
 
 @RestController
 public class GreetingsController {
-	
-	@RequestMapping(path="/hello-world", method=RequestMethod.GET)
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@RequestMapping(path = "/hello-world", method = RequestMethod.GET)
 	public String helloWorld() {
 		return "Hello World !!";
 	}
-	
+
 	@GetMapping(path = "/good-bye")
 	public String goodBye() {
 		return "Good Bye !!";
 	}
-	
+
 	@GetMapping(path = "/good-morning")
 	public GreetingsResponse goodMorning() {
 		return new GreetingsResponse("Good Morning !!");
 	}
-	
+
 	@GetMapping(path = "/greet/{message}")
 	public GreetingsResponse greet(@PathVariable String message) {
 		return new GreetingsResponse(message);
+	}
+
+	//Use LocaleContextHolder to avoid passing accept-language header in every request
+	@GetMapping(path = "/good-morning-i18n")
+	public String goodMorningWithI18n(/*@RequestHeader(name = "accept-language", required = false) Locale locale*/) {
+		return messageSource.getMessage("good.morining", null, "Default-Message", LocaleContextHolder.getLocale());
 	}
 }
