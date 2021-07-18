bd27d84563fb1e8dcb09535c74e91debe9039a0f
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2021-07-18 15:57:25 +0530

    Setting up H2 Data base for JPA

--- microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/model/User.java
+++ microservices/restwebservices/src/main/java/com/courses/microservices/restwebservices/model/User.java
@@ -5,12 +5,19 @@ package com.courses.microservices.restwebservices.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author Aniruddh Mishra
  *
  */
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String name;
 	private LocalDate birthDate;
---
+++ microservices/restwebservices/src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'John');
+insert into user values(2, sysdate(), 'Rick');
+insert into user values(3, sysdate(), 'Jerry');
