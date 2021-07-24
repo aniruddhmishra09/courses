d517d0aaa44ddceb59b2e87b26faa5a146c90bbf
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2021-07-24 18:40:26 +0530

    First Version of Limit Service

---
+++ microservices/ms-with-spring-boot-2_4_or_higher/system-limits/src/main/java/com/courses/microservices/systemlimits/SystemLimitsApplication.java
@@ -0,0 +1,13 @@
+package com.courses.microservices.systemlimits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLimitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLimitsApplication.class, args);
+	}
+
+}
---
+++ microservices/ms-with-spring-boot-2_4_or_higher/system-limits/src/test/java/com/courses/microservices/systemlimits/SystemLimitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.microservices.systemlimits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemLimitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
