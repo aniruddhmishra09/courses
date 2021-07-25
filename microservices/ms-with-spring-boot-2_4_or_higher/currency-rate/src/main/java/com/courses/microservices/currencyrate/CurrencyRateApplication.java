adf2179fdd9f37dad453662978c3ad88f80dc929
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2021-07-25 17:40:31 +0530

    First Version of currency-rate service

---
+++ microservices/ms-with-spring-boot-2_4_or_higher/currency-rate/src/main/java/com/courses/microservices/currencyrate/CurrencyRateApplication.java
@@ -0,0 +1,13 @@
+package com.courses.microservices.currencyrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateApplication.class, args);
+	}
+
+}
---
+++ microservices/ms-with-spring-boot-2_4_or_higher/currency-rate/src/test/java/com/courses/microservices/currencyrate/CurrencyRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.microservices.currencyrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
