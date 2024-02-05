36cf2d75e76a80422f4397e0c88cef2c333a811f
Aniruddh Mishra <aniruddhmishra@Aniruddhs-MacBook-Pro.local>
2024-02-05 23:55:13 +0530

    First Kafka Commit

---
+++ kafka/kafka-core/src/main/java/com/courses/kafka/producer/SimpleProducer.java
@@ -0,0 +1,14 @@
+package com.courses.kafka.producer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SimpleProducer {
+
+    public static void main(String[] args) {
+        log.info("inside");
+        System.out.println("Hello");
+    }
+
+
+}
