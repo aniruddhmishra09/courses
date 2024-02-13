05afaa7a1f80a4b20d1ef2a35ee538d583b54f9a
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2024-02-13 08:34:43 +0530

    First Kafka Producer

--- kafka/kafka-core/src/main/java/com/courses/kafka/producer/SimpleProducer.java
+++ kafka/kafka-core/src/main/java/com/courses/kafka/producer/SimpleProducer.java
@@ -1,15 +1,33 @@
 package com.courses.kafka.producer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
 
 @Slf4j
 public class SimpleProducer {
 
     public static void main(String[] args) {
-        log.info("inside");
-        System.out.println("Hello");
+        log.info("Entering Kafka Producer");
+        String topic = "core-kafka-1";
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
 
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+        try(KafkaProducer<String, String> producer = new KafkaProducer<>(properties);){
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, "MyValue");
+            log.info("Before Sending Message to Kafka Topic - {}",topic);
+            producer.send(record);
+            log.info("After Sending Message to Kafka Topic - {}",topic);
+        }catch (Exception ex){
+            log.error("Error while publishing message",ex);
+        }
+
+        log.info("Exiting Kafka Producer");
     }
-
-
 }
