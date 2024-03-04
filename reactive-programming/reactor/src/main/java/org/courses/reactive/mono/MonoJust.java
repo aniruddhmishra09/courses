9e5bfeb2bf884bcfeb46297d1e33fc4af6aa0d1d
aniruddhmishra09 <aniruddh.mishra@outlook.com>
2024-03-04 21:47:41 +0530

    Adding Mono-Just Demo

---
+++ reactive-programming/reactor/src/main/java/org/courses/reactive/mono/MonoJust.java
@@ -0,0 +1,14 @@
+package org.courses.reactive.mono;
+
+import reactor.core.publisher.Mono;
+
+public class MonoJust {
+
+    public static void main(String[] args) {
+        //Publisher
+        Mono<Integer> mono = Mono.just(1);
+        System.out.println(mono);
+        //Subscribing Mono - In Order to Publisher to emmit data , you have to subscribe
+        mono.subscribe(i -> System.out.println("Received - "+i));
+    }
+}
